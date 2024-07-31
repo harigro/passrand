f95b97d1277799fa94751ce73918420dcd7e1543
harigro <trierbank@gmail.com>
2024-07-31 08:36:09 +0700

    mengganti judul jendela

--- units/ui.py
+++ units/ui.py
@@ -17,117 +17,280 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(610, 443)
+        Form.resize(926, 850)
         self.gridLayout = QGridLayout(Form)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.gridLayout.addItem(self.horizontalSpacer, 1, 0, 1, 1)
+        self.gridLayout.addItem(self.horizontalSpacer_2, 1, 2, 1, 1)
+
+        self.frame = QFrame(Form)
+        self.frame.setObjectName(u"frame")
+        self.frame.setMinimumSize(QSize(600, 400))
+        self.frame.setSizeIncrement(QSize(20, 20))
+        self.frame.setBaseSize(QSize(0, 0))
+        self.frame.setFrameShape(QFrame.NoFrame)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout = QVBoxLayout(self.frame)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.frame_14 = QFrame(self.frame)
+        self.frame_14.setObjectName(u"frame_14")
+        self.frame_14.setFrameShape(QFrame.NoFrame)
+        self.frame_14.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.frame_14)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.label = QLabel(self.frame_14)
+        self.label.setObjectName(u"label")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_2.addWidget(self.label)
+
+
+        self.verticalLayout.addWidget(self.frame_14)
+
+        self.frame_13 = QFrame(self.frame)
+        self.frame_13.setObjectName(u"frame_13")
+        self.frame_13.setFrameShape(QFrame.NoFrame)
+        self.frame_13.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_11 = QHBoxLayout(self.frame_13)
+        self.horizontalLayout_11.setObjectName(u"horizontalLayout_11")
+        self.lineEdit = QLineEdit(self.frame_13)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.horizontalLayout_11.addWidget(self.lineEdit)
+
+
+        self.verticalLayout.addWidget(self.frame_13)
+
+        self.frame_2 = QFrame(self.frame)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.NoFrame)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.formLayout = QFormLayout(self.frame_2)
+        self.formLayout.setObjectName(u"formLayout")
+        self.frame_3 = QFrame(self.frame_2)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout = QHBoxLayout(self.frame_3)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label_2 = QLabel(self.frame_3)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout.addWidget(self.label_2)
+
+
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.frame_3)
+
+        self.frame_4 = QFrame(self.frame_2)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_4)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalSlider = QSlider(self.frame_4)
+        self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setMaximum(25)
+        self.horizontalSlider.setOrientation(Qt.Horizontal)
+
+        self.horizontalLayout_2.addWidget(self.horizontalSlider)
+
+        self.label_7 = QLabel(self.frame_4)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setAlignment(Qt.AlignCenter)
+
+        self.horizontalLayout_2.addWidget(self.label_7)
+
+
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.frame_4)
+
+        self.frame_5 = QFrame(self.frame_2)
+        self.frame_5.setObjectName(u"frame_5")
+        self.frame_5.setFrameShape(QFrame.StyledPanel)
+        self.frame_5.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.frame_5)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label_3 = QLabel(self.frame_5)
+        self.label_3.setObjectName(u"label_3")
+
+        self.horizontalLayout_3.addWidget(self.label_3)
+
+
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.frame_5)
+
+        self.frame_6 = QFrame(self.frame_2)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setFrameShape(QFrame.StyledPanel)
+        self.frame_6.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.frame_6)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.checkBox = QCheckBox(self.frame_6)
+        self.checkBox.setObjectName(u"checkBox")
+
+        self.horizontalLayout_4.addWidget(self.checkBox)
+
+        self.horizontalSpacer_7 = QSpacerItem(60, 20, QSizePolicy.Preferred, QSizePolicy.Minimum)
+
+        self.horizontalLayout_4.addItem(self.horizontalSpacer_7)
+
+        self.label_8 = QLabel(self.frame_6)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setAlignment(Qt.AlignCenter)
+
+        self.horizontalLayout_4.addWidget(self.label_8)
+
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_4.addItem(self.horizontalSpacer_3)
+
+
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.frame_6)
+
+        self.frame_7 = QFrame(self.frame_2)
+        self.frame_7.setObjectName(u"frame_7")
+        self.frame_7.setFrameShape(QFrame.StyledPanel)
+        self.frame_7.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_5 = QHBoxLayout(self.frame_7)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_4 = QLabel(self.frame_7)
+        self.label_4.setObjectName(u"label_4")
+
+        self.horizontalLayout_5.addWidget(self.label_4)
+
+
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.frame_7)
+
+        self.frame_8 = QFrame(self.frame_2)
+        self.frame_8.setObjectName(u"frame_8")
+        self.frame_8.setFrameShape(QFrame.StyledPanel)
+        self.frame_8.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_6 = QHBoxLayout(self.frame_8)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.checkBox_2 = QCheckBox(self.frame_8)
+        self.checkBox_2.setObjectName(u"checkBox_2")
+
+        self.horizontalLayout_6.addWidget(self.checkBox_2)
+
+        self.horizontalSpacer_8 = QSpacerItem(60, 20, QSizePolicy.Preferred, QSizePolicy.Minimum)
+
+        self.horizontalLayout_6.addItem(self.horizontalSpacer_8)
+
+        self.label_9 = QLabel(self.frame_8)
+        self.label_9.setObjectName(u"label_9")
+
+        self.horizontalLayout_6.addWidget(self.label_9)
+
+        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_6.addItem(self.horizontalSpacer_4)
+
+
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.frame_8)
+
+        self.frame_9 = QFrame(self.frame_2)
+        self.frame_9.setObjectName(u"frame_9")
+        self.frame_9.setFrameShape(QFrame.StyledPanel)
+        self.frame_9.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_7 = QHBoxLayout(self.frame_9)
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.label_5 = QLabel(self.frame_9)
+        self.label_5.setObjectName(u"label_5")
+
+        self.horizontalLayout_7.addWidget(self.label_5)
+
+
+        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.frame_9)
+
+        self.frame_10 = QFrame(self.frame_2)
+        self.frame_10.setObjectName(u"frame_10")
+        self.frame_10.setFrameShape(QFrame.StyledPanel)
+        self.frame_10.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_8 = QHBoxLayout(self.frame_10)
+        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
+        self.checkBox_3 = QCheckBox(self.frame_10)
+        self.checkBox_3.setObjectName(u"checkBox_3")
+
+        self.horizontalLayout_8.addWidget(self.checkBox_3)
+
+        self.horizontalSpacer_9 = QSpacerItem(60, 20, QSizePolicy.Preferred, QSizePolicy.Minimum)
+
+        self.horizontalLayout_8.addItem(self.horizontalSpacer_9)
+
+        self.label_10 = QLabel(self.frame_10)
+        self.label_10.setObjectName(u"label_10")
+
+        self.horizontalLayout_8.addWidget(self.label_10)
+
+        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_8.addItem(self.horizontalSpacer_5)
+
+
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.frame_10)
+
+        self.frame_11 = QFrame(self.frame_2)
+        self.frame_11.setObjectName(u"frame_11")
+        self.frame_11.setFrameShape(QFrame.StyledPanel)
+        self.frame_11.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_9 = QHBoxLayout(self.frame_11)
+        self.horizontalLayout_9.setObjectName(u"horizontalLayout_9")
+        self.label_6 = QLabel(self.frame_11)
+        self.label_6.setObjectName(u"label_6")
+
+        self.horizontalLayout_9.addWidget(self.label_6)
+
+
+        self.formLayout.setWidget(4, QFormLayout.LabelRole, self.frame_11)
+
+        self.frame_12 = QFrame(self.frame_2)
+        self.frame_12.setObjectName(u"frame_12")
+        self.frame_12.setFrameShape(QFrame.StyledPanel)
+        self.frame_12.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_10 = QHBoxLayout(self.frame_12)
+        self.horizontalLayout_10.setObjectName(u"horizontalLayout_10")
+        self.checkBox_4 = QCheckBox(self.frame_12)
+        self.checkBox_4.setObjectName(u"checkBox_4")
+
+        self.horizontalLayout_10.addWidget(self.checkBox_4)
+
+        self.horizontalSpacer_10 = QSpacerItem(60, 20, QSizePolicy.Preferred, QSizePolicy.Minimum)
+
+        self.horizontalLayout_10.addItem(self.horizontalSpacer_10)
+
+        self.label_11 = QLabel(self.frame_12)
+        self.label_11.setObjectName(u"label_11")
+
+        self.horizontalLayout_10.addWidget(self.label_11)
+
+        self.horizontalSpacer_6 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_10.addItem(self.horizontalSpacer_6)
+
+
+        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.frame_12)
+
+
+        self.verticalLayout.addWidget(self.frame_2)
+
+        self.pushButton = QPushButton(self.frame)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+
+        self.gridLayout.addWidget(self.frame, 1, 1, 1, 1)
 
         self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
         self.gridLayout.addItem(self.verticalSpacer_2, 2, 1, 1, 1)
 
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.gridLayout.addItem(self.horizontalSpacer_2, 1, 2, 1, 1)
+        self.gridLayout.addItem(self.horizontalSpacer, 1, 0, 1, 1)
 
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
         self.gridLayout.addItem(self.verticalSpacer, 0, 1, 1, 1)
-
-        self.frame = QFrame(Form)
-        self.frame.setObjectName(u"frame")
-        self.frame.setMinimumSize(QSize(400, 400))
-        self.frame.setBaseSize(QSize(400, 0))
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-        self.label = QLabel(self.frame)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(20, 10, 131, 16))
-        self.lineEdit = QLineEdit(self.frame)
-        self.lineEdit.setObjectName(u"lineEdit")
-        self.lineEdit.setGeometry(QRect(10, 50, 371, 41))
-        self.frame_2 = QFrame(self.frame)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setGeometry(QRect(0, 109, 401, 241))
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
-        self.formLayout = QFormLayout(self.frame_2)
-        self.formLayout.setObjectName(u"formLayout")
-        self.label_2 = QLabel(self.frame_2)
-        self.label_2.setObjectName(u"label_2")
-
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label_2)
-
-        self.horizontalSlider = QSlider(self.frame_2)
-        self.horizontalSlider.setObjectName(u"horizontalSlider")
-        self.horizontalSlider.setMaximum(25)
-        self.horizontalSlider.setOrientation(Qt.Horizontal)
-
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.horizontalSlider)
-
-        self.label_3 = QLabel(self.frame_2)
-        self.label_3.setObjectName(u"label_3")
-
-        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_3)
-
-        self.checkBox = QCheckBox(self.frame_2)
-        self.checkBox.setObjectName(u"checkBox")
-
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.checkBox)
-
-        self.label_4 = QLabel(self.frame_2)
-        self.label_4.setObjectName(u"label_4")
-
-        self.formLayout.setWidget(4, QFormLayout.LabelRole, self.label_4)
-
-        self.checkBox_2 = QCheckBox(self.frame_2)
-        self.checkBox_2.setObjectName(u"checkBox_2")
-
-        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.checkBox_2)
-
-        self.label_5 = QLabel(self.frame_2)
-        self.label_5.setObjectName(u"label_5")
-
-        self.formLayout.setWidget(6, QFormLayout.LabelRole, self.label_5)
-
-        self.checkBox_3 = QCheckBox(self.frame_2)
-        self.checkBox_3.setObjectName(u"checkBox_3")
-
-        self.formLayout.setWidget(6, QFormLayout.FieldRole, self.checkBox_3)
-
-        self.label_6 = QLabel(self.frame_2)
-        self.label_6.setObjectName(u"label_6")
-
-        self.formLayout.setWidget(8, QFormLayout.LabelRole, self.label_6)
-
-        self.checkBox_4 = QCheckBox(self.frame_2)
-        self.checkBox_4.setObjectName(u"checkBox_4")
-
-        self.formLayout.setWidget(8, QFormLayout.FieldRole, self.checkBox_4)
-
-        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.formLayout.setItem(1, QFormLayout.FieldRole, self.verticalSpacer_3)
-
-        self.verticalSpacer_4 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.formLayout.setItem(3, QFormLayout.FieldRole, self.verticalSpacer_4)
-
-        self.verticalSpacer_5 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.formLayout.setItem(5, QFormLayout.FieldRole, self.verticalSpacer_5)
-
-        self.verticalSpacer_6 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.formLayout.setItem(7, QFormLayout.FieldRole, self.verticalSpacer_6)
-
-        self.pushButton = QPushButton(self.frame)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(10, 360, 371, 32))
-
-        self.gridLayout.addWidget(self.frame, 1, 1, 1, 1)
 
 
         self.retranslateUi(Form)
@@ -136,17 +299,22 @@ class Ui_Form(object):
     # setupUi
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        self.label.setText(QCoreApplication.translate("Form", u"Generator Kata Sandi", None))
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"PassRand", None))
+        self.label.setText(QCoreApplication.translate("Form", u"Penghasil Kata Sandi", None))
         self.label_2.setText(QCoreApplication.translate("Form", u"Jumlah Karakter  ", None))
+        self.label_7.setText("")
         self.label_3.setText(QCoreApplication.translate("Form", u"Huruf Besar          ", None))
         self.checkBox.setText("")
+        self.label_8.setText(QCoreApplication.translate("Form", u"ABCDEFGHIJKLMNOPQRSTUVWXYZ", None))
         self.label_4.setText(QCoreApplication.translate("Form", u"Huruf Kecil", None))
         self.checkBox_2.setText("")
+        self.label_9.setText(QCoreApplication.translate("Form", u"abcdefghijklmnopqrstuvwxyz", None))
         self.label_5.setText(QCoreApplication.translate("Form", u"Bilangan", None))
         self.checkBox_3.setText("")
+        self.label_10.setText(QCoreApplication.translate("Form", u"0123456789", None))
         self.label_6.setText(QCoreApplication.translate("Form", u"Simbol", None))
         self.checkBox_4.setText("")
+        self.label_11.setText(QCoreApplication.translate("Form", u"!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", None))
         self.pushButton.setText(QCoreApplication.translate("Form", u"Generator Kata Sandi", None))
     # retranslateUi
 
