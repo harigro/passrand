8d14ccbee93dd505ada11946673b739827eecb95
harigro <trierbank@gmail.com>
2024-07-31 08:34:55 +0700

    menambahkan paket style

--- main.py
+++ main.py
@@ -2,25 +2,20 @@ import sys
 from typing import Callable, List, Dict
 from PySide2.QtWidgets import QApplication, QWidget
 from PySide2.QtGui import QIcon
-from units.ui import Ui_Form  # Ganti dengan nama modul yang sesuai
-from qt_material import apply_stylesheet
+from units.ui import Ui_Form
+from style.karbon import karbon
 from units.rand_string import RandString
 from units.kombinasi import kombinasi_satu, kombinasi_dua, kombinasi_tiga
 
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
-
-        # Set up the user interface from Designer.
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-
         # Mengatur ikon aplikasi
-        self.setWindowIcon(QIcon('assets/kunci.png'))  
-
+        self.setWindowIcon(QIcon('assets/kunci.png'))
         # inisialisasi untuk menyimpan nilai dari slider
         self.nilai: int = 0
-
         # inisialisai dict untuk menyimpan nilai bool
         self.data: Dict[str, bool] = {
             'checkBox': False,
@@ -28,7 +23,6 @@ class MyApp(QWidget):
             'checkBox_3': False,
             'checkBox_4': False,
         }
-        
         # Inisialisasi dictionary untuk menyimpan checkBox
         self.check_boxes: Dict[str, bool] = {
             'checkBox': self.ui.checkBox,
@@ -36,18 +30,14 @@ class MyApp(QWidget):
             'checkBox_3': self.ui.checkBox_3,
             'checkBox_4': self.ui.checkBox_4,
         }
-
         # Hubungkan sinyal stateChanged dari semua checkBox ke metode self.checkbox_state_changed
         for checkbox in self.check_boxes.values():
             checkbox.stateChanged.connect(self.checkbox_state_changed)
-
         # Hubungkan sinyal valueChanged dari horizontalSlider ke metode self.slider_value_changed
         self.ui.horizontalSlider.valueChanged.connect(self.slider_value_changed)
-
         # Atur nilai minimum dan maksimum untuk horizontalSlider
         self.ui.horizontalSlider.setMinimum(0)
-        self.ui.horizontalSlider.setMaximum(12)
-
+        self.ui.horizontalSlider.setMaximum(15)
         #  atur baris edit hanya baca
         self.ui.lineEdit.setReadOnly(True)
         # Hubungkan sinyal clicked dari pushButton ke metode self.saat_ditekan
@@ -56,10 +46,8 @@ class MyApp(QWidget):
     def checkbox_state_changed(self, state):
         # Mendapatkan checkbox yang menyebabkan perubahan
         sender_checkbox = self.sender()
-
         # Mendapatkan nama checkbox
         checkbox_name = next(name for name, checkbox in self.check_boxes.items() if checkbox == sender_checkbox)
-
         if checkbox_name:
             # Mendapatkan nilai aktual checkbox
             checkbox_value = sender_checkbox.isChecked()
@@ -68,13 +56,12 @@ class MyApp(QWidget):
     def slider_value_changed(self, value):
         # Mendapatkan nilai aktual dari horizontalSlider
         self.nilai = value
+        self.ui.label_7.setText(str(value)) 
 
     def saat_ditekan(self):
         vv: List[bool] = list(self.data.values())  # Menggunakan List[bool] daripada Iterable[bool] untuk vv
-
         # Mendefinisikan tipe data untuk parameter dan nilai kembalian
         baris: Callable[[str, str], None] = lambda p, q: self.ui.lineEdit.setText(RandString(p, q))
-
         # kode akan berjalan jika self.nilai lebih dari 0
         if self.nilai > 0:
             if all(vv):
@@ -114,7 +101,7 @@ class MyApp(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyleSheet(karbon)
     window = MyApp()
     window.show()
-    apply_stylesheet(app, theme='light_blue.xml')
     sys.exit(app.exec_())
